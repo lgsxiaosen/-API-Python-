8c75065587933ee7fa529a85651ecebe4c709cd6
thefishforgetdrink <thefishforgetdrink@users.noreply.github.com>
2017-03-09 15:15:54 +0800

    Add files via upload

---
+++ python_test/crawl_test.py
@@ -0,0 +1,52 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    gsliu 2017-02-28
+    
+"""
+from bs4 import BeautifulSoup
+from urlparse import urljoin
+import requests
+import csv
+
+url = "http://bj.58.com/pinpaigongyu/pn/{page}/?minprice=1500_2000"
+
+# 已完成的页数序号，初时为0
+page = 0
+# 打开rent.csv文件
+csv_file = open("rent.csv", "wb")
+# 创建writer对象，指定文件与分隔符
+csv_writer = csv.writer(csv_file, delimiter=',')
+
+while True:
+    page += 1
+    print "fetch: ", url.format(page=page)
+    # 抓取目标页面
+    response = requests.get(url.format(page=page))
+    # 创建一个BeautifulSoup对象,获取页面正文
+    html = BeautifulSoup(response.text)
+    # 获取class=list的元素下的所有li元素
+    house_list = html.select(".list > li")
+
+    # 循环在读不到新的房源时结束
+    if not house_list:
+        break
+
+    for house in house_list:
+        # 得到标签包裹着的文本
+        house_title = house.select("h2")[0].string.encode("utf8")
+        # 得到标签内属性的值
+        house_url = urljoin(url, house.select("a")[0]["href"])
+        house_info_list = house_title.split()
+
+        # 如果第二列是公寓名则取第一列作为地址
+        if "公寓" in house_info_list[1] or "青年社区" in house_info_list[1]:
+            house_location = house_info_list[0]
+        else:
+            house_location = house_info_list[1]
+
+        house_money = house.select(".money")[0].select("b")[0].string.encode("utf8")
+        # 写一行数据
+        csv_writer.writerow([house_title, house_location, house_money, house_url])
+
+csv_file.close()
